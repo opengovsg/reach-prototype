59355c20d1de1b9e67d9b856b932974c964a9959
hanu-ogp <127919022+hanu-ogp@users.noreply.github.com>
2023-11-06 13:47:49 +0800

    chore: feedback type validation

--- src/schemas/feedback.ts
+++ src/schemas/feedback.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 export const submitFeedbackSchema = z.object({
   name: z.string().min(1).max(50),
   email: z.string().email(),
-  contactNumber: z.string().min(1).max(8).optional(),
-  subject: z.string().min(1).max(50),
-  feedbackDetail: z.string().min(1).max(5000),
+  contactNumber: z.string().min(1).max(15).optional(),
+  subject: z.string().min(1).max(100),
+  feedbackDetail: z.string().min(1).max(5010),
 })
 export type submitFeedbackInput = z.infer<typeof submitFeedbackSchema>
